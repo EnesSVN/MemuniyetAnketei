6742ba7e79ce287c4a0b9c589c518e33f90d6995
giriftzen <enes.seveng37@gmail.com>
2022-07-06 11:46:21 +0200

    [Add] Repository>AnketRepository

---
+++ src/main/java/com/example/memnuniyetanketi/Repository/AnketRepository.java
@@ -0,0 +1,9 @@
+package com.example.memnuniyetanketi.Repository;
+
+import com.example.memnuniyetanketi.Model.Anket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnketRepository extends JpaRepository<Anket, Long> {
+
+
+}
